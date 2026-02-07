7ce2a90b6096d277e945c14d4ae63fc613248336
Vlad T <4170663+vladt89@users.noreply.github.com>
2026-02-07 14:22:35 +0200

    Extract hard coded merchant values to separate file

---
+++ src/resources/default-categories.ts
@@ -0,0 +1,293 @@
+export const FOOD_SHOPS_SHORT_NAMES = [
+    "ALEPA",
+    "LIDL",
+    "PRISMA",
+    "K-supermarket",
+    "K-market",
+    "S-Market",
+    "K-Citymarket",
+    "K market",
+    "MINIMARKET",
+    "S MARKET",
+    "LENTA",
+    "EDEKA",
+    "PEREKRESTOK",
+    "DISAS",
+    "Netto",
+    "KAUFLAND",
+    "ALDI", // german food shops
+    "RIMI", // estonian food shop
+    "REWE",
+    "MERCADONA",
+    "SUPERMERCADOS",
+    "ELLI",
+    "ZABKA", // Polish supermarket
+    "CONSUM",
+    "PENNY",
+];
+
+export const HOUSE_SHOPS_SHORT_NAMES = [
+    "Asunto Oy Kuparikartano",
+    "IKEA",
+    "K-Rauta",
+    "Helen Oy",
+    "TIKHOMIROV V TAI WEINER C",
+    "Elisa Oyj",
+    "BAUHAUS",
+    "BAUMARKT",
+    "MIKKO UGOLNIKOV",
+    "Gigantti",
+    "DNA", // internet
+    "BILTEMA",
+    "AURORANLINNA", // renting
+    "HOUSINGANY", // Berlin renting
+    "KUMAR RAJU", // bed
+    "LVI PALVELU TUKIAINEN", // Hana installation
+    "Von Haugwitz Gotthard", // Lippstadt renting
+    "Vodafone",
+    "ACTION 3038", // Erwitte
+    "MONTANA", // electricity
+    "MEDIA MARKT",
+    "Rundfunk ARD, ZDF, DRadio",
+    "ROSSMANN",
+    "JYSK",
+    "MEDIAMARKT",
+    "ARTUR KARAZANOV",
+    "Miete",
+    "PremiumSIM", // handy
+];
+
+export const KIDS_FAMILY_NAMES = [
+    "Phoenix Partners Ky/LaughLearn",
+    "MUSTI JA MIRRI",
+    "VETKLINIKA VOLF",
+    "EVGENIJA KRUGLOVA",
+    "PERHEKESKUS MARIA RY",
+    "Seikkailupuisto Korkee",
+    "HIIHTOSEURA",
+    "HOPLOP",
+    "Voimisteluseura",
+    "TWI SERVICES OY", // Russian visa
+    "CHESS GALAXY",
+    "HELSINGIN UIMARIT",
+    "Christina Weiner FI",
+    "HGIN KAUPUNKI/ TALPA/LASKUTUS", // продленка
+    "BADMINTON CLUB",
+    "Cetus Espoo ry", // swim
+    "IVANCHSHICOVA",
+    "Yelena Simonenko",
+    "KORKEASAAREN ELAIN",
+    "SUOMISPORT", // chess license for Leo
+    "Christina Weiner", // aliments
+    "HELSINGIN KAUPUNKI", // продленка
+    "Perhekeskus Maria",
+];
+
+export const SPORT_FOOD_FUN_NAMES = [
+    "TALIHALLI",
+    "ACTIVE GROUP RY",
+    "VFI*Rami's Coffee Oy",
+    "Inna Repo",
+    "Asian Fusion Oy",
+    "SEIKKAILUPUISTO ZIPPY",
+    "INTER RAVINTOLA",
+    "INTER PIZZA",
+    "Electrobike",
+    "XXL",
+    "RESTAURANT",
+    "PoplaCandy",
+    "ABC",
+    "BK KONALA",
+    "CAFE",
+    "CITY OF HELSINKI",
+    "Wilhelm Breimaier",
+    "Intelinvest",
+    "RAVINTOLA",
+    "PIZZERIA",
+    "Eat Poke Hki Oy",
+    "HIEKKA BEACHSPOT OY",
+    "HSBIKEDISCO",
+    "DIGICLOUDTR",
+    "Taste Creator Oy",
+    "HESBURGER",
+    "OCR Factory",
+    "IGELS",
+    "TONI PITKANEN",
+    "Kiipeilyvisio Oy",
+    "FREE MOTION",
+    "BAR",
+    "VIHTI SKI CENTE",
+    "MOTHER INDIA",
+    "LAGER 157",
+    "BURGER KING",
+    "Messila Maailma",
+    "NATURA VIVA",
+    "Subway",
+    "FINNKINO",
+    "RESTORAN",
+    "SHERLOCK HOLMES",
+    "NOODLE STORY OY",
+    "DONER",
+    "Boneless",
+    "POPLATEK",
+    "UIMA",
+    "IRONMAN",
+    "EO SPACE OY", // School challenge RU
+    "ZWIFT",
+    "CYCLECLASSI", // rent bike
+    "Cycle Classic", // rent bike
+    "Rush",
+    "Serena",
+    "MCDONALDS",
+    "KRISPY KEBAB",
+    "STADIUM",
+    "Aalto Group Oy", // Varuste.net
+    "Helsingin kaupunki Pirkko", // Swimming pool
+    "ROCKTHESPORT", // Marathon
+    "Starcart", // watch brother
+    "MCD", // MacDuck
+    "RAMEN",
+    "BAECKEREI",
+    "BACKEREI",
+    "RISTORANTE",
+    "PIZZA",
+    "PETERS PRALINCHEN",
+    "KEBAB",
+    "KEBAP",
+    "PRALINENSHOP PETERS",
+    "Lippstaedter Turnverein",
+    "BACKHAUS LIENING",
+    "SUEWOLTO BECKUMER",
+    "CABRIOLI",
+    "SOLBAD",
+    "GOLD WOK",
+    "Alpine",
+    "DECATHLON",
+    "TSAGHKADZOR ROPEWAY",
+];
+
+export const CAR_TRANSPORT_SHOPS_SHORT_NAMES = [
+    "NESTE",
+    "HSL",
+    "HELPPOKATSASTUS",
+    "PARKMAN",
+    "Parking",
+    "TANKSTELLE",
+    "AIMO PARK",
+    "Teboil",
+    "SHELL",
+    "LansiAuto",
+    "ODNO KOLESO",
+    "TANKSTATION",
+    "Aral Station",
+    "TRAFICOM",
+    "SHELL",
+    "LIPPUAUTOMAATIT",
+    "Motonet",
+    "AMZN Mktp US*HY5EI2EQ0",
+    "HELSINKI/KYMP/PYSÄKÖINTI", // paring fine
+    "AUTOBAHN TANK",
+    "PETROL",
+    "TAXI",
+    "EASYPARK",
+    "BOLT",
+    "VOBA BECKUM-LIPPSTADT",
+    "JORMA AULIS HALON", // bought skoda
+    "METRO",
+    "UBER",
+    "TOTAL SERVICE STATION",
+    "STAR",
+    "ESSO STATION", // petrol station
+    "PARKSERVICE",
+    "A.T.U.",
+    "fair parken GmbH",
+    "JET",
+];
+
+export const TRAVEL_NAMES = [
+    "VIKING LINE",
+    "Tallink",
+    "FINNLADY",
+    "FINNLINES",
+    "Hotel",
+    "BOLT",
+    "PAYTRAIL",
+    "DIRECTF",
+    "MOTEL",
+    "RENT A CAR",
+    "RAILW",
+    "CORENDONAIRLINES",
+    "FINNAIR",
+    "SAMUEL LINDBLOM",
+    "Milano Portello",
+    "Trustly Group AB", // finnair
+    "Scandic Skarholmen FO",
+    "SCANDLINES DEUTSCHLAND GM",
+    "CINDERELLA",
+    "VASAMUSEET",
+    "VISIT VADSTENA",
+    "OERESUNDSBROEN", // Denmark-Sweden bridge
+    "BBR ORGANIZASYON REKLAM",
+    "ALANYA TELEFERIK",
+    "ANTALYA",
+    "ISTANBUL",
+    "ALANYA",
+    "BOOKING",
+    "Ilia Tumkin", // travel (for parents) or money for Russia
+    "LUFTHANSA",
+    "LuxExpress",
+    "BALTICSHUTTLE",
+    "ECOLINES", // buses to Russia
+    "VIKINGLINE",
+    "ECKERO LINE",
+    "DIRECT FERR", // ferries to Tallinn
+    "AIRBNB",
+    "DBVERTRIEBG A",
+    "DB Vertrieb", // Deutsche Bahn
+    "WIZZ AIR",
+    "AIRBALTIC",
+    "elron", // Estonian train
+    "KALEVSPA",
+    "GKD GLOBAL FZE LLC", // Russian e-visa
+    "RUSTRAVEL",
+    "HOSTAL",
+    "VISA ZW RENOMA SP ZOO", // Gdansk transport
+    "MUZEUM",
+    "WIZZAIR",
+    "FLIXBUS",
+    "BVG",
+    "FlixTrain",
+    "FLUGLADEN",
+    "Holiday Inn",
+    "Museum",
+];
+
+export const HEALTH_NAMES = [
+    "TERVEYSTALO MYYRMAKI",
+    "Specsavers",
+    "Malminkartanon apteekki",
+    "CENTR KORREKCII ZRENIYA",
+    "APTEKA",
+    "SILMAASEMA",
+    "HUS",
+    "APOTHEKE",
+    "ELAINSAIRAALA",
+    "Apteekki",
+    "Myyrmannin apt",
+    "Fysio Sakura",
+    "apteek",
+    "FARMACIA",
+    "MISTER SPEX", // contact lenses
+];
+
+export const INSURANCE_NAMES = [
+    "POHJOLA VAKUUTUS OY",
+    "IF VAKUUTUS",
+    "AOK",
+    "Versicherungs",
+];
+
+export const INVEST_NAMES = [
+    "Interactive Brokers",
+    "Bitstamp",
+];
